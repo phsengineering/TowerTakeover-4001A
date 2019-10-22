a05ef24abe49651bcfd5f2c108b4fa44ac1a2b5b
javathunderman <javathunderman@outlook.com>
2019-10-21 22:50:04 -0400

    include proposed refactor

--- src/auton.cpp
+++ src/auton.cpp
@@ -3,19 +3,9 @@ void autonhandler() {
   clearDrive();
   smartDrive(300, 2500.0);
   smartDrive(-300, -600.0);
-  while(gyro.get_value() > -250) {
-    driveRF.move_velocity(150);
-    driveRB.move_velocity(150);
-    driveLF.move_velocity(-150);
-    driveLB.move_velocity(-150);
-  }
+  counterclockwise(250, 150);
   smartDrive(-710, -980.0);
-  while(gyro.get_value() < 7.5) {
-    driveRF.move_velocity(-150);
-    driveRB.move_velocity(-150);
-    driveLF.move_velocity(150);
-    driveLB.move_velocity(150);
-  }
+  clockwise(7, 150);
   driveRF.move_velocity(150);
   driveRB.move_velocity(150);
   driveLF.move_velocity(-150);
@@ -25,21 +15,10 @@ void autonhandler() {
   intakeHandler(195);
   smartDrive(-300, -1550);
   gyro.reset();
-  while(gyro.get_value() < 690) {
-    driveRF.move_velocity(-150);
-    driveRB.move_velocity(-150);
-    driveLF.move_velocity(150);
-    driveLB.move_velocity(150);
-  }
-  driveRF.move_velocity(.4*300);
-  driveRB.move_velocity(.4*300);
-  driveLF.move_velocity(.4*300);
-  driveLB.move_velocity(.4*300);
+  clockwise(690, 150);
+  driveVel(.4*300);
   pros::delay(500);
-  driveRF.move_velocity(0);
-  driveRB.move_velocity(0);
-  driveLF.move_velocity(0);
-  driveLB.move_velocity(0);
+  driveVel(0);
   intakeHandler(-75);
   pros::delay(500);
   intakeHandler(0);
--- src/main.cpp
+++ src/main.cpp
@@ -17,13 +17,13 @@ void opcontrol() {
 		pros::Controller mainController = Controller(E_CONTROLLER_MASTER);
 		autonhandler();
 		pros::lcd::initialize();
-		lift.set_brake_mode(E_MOTOR_BRAKE_COAST);
-		tray.set_brake_mode(E_MOTOR_BRAKE_COAST);
+		set_brake(1, lift);
+		set_brake(1, tray);
 		int macroHandler = 0;
 		int macroHandler2 = 0;
 		gyro.reset();
 		while(true) {
-			puts(std::to_string(gyro.get_value()).c_str());
+			get_gyro();
 			if(lift.get_temperature() > 55.0) {
 				mainController.rumble(". -");
 			}
@@ -42,17 +42,17 @@ void opcontrol() {
 			}
 			if(mainController.get_digital(E_CONTROLLER_DIGITAL_A))
 			{
-				driveLF.set_brake_mode(E_MOTOR_BRAKE_HOLD);
-				driveLB.set_brake_mode(E_MOTOR_BRAKE_HOLD);
-				driveRF.set_brake_mode(E_MOTOR_BRAKE_HOLD);
-				driveRB.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+				set_brake(0, driveLF);
+				set_brake(0, driveLB);
+				set_brake(0, driveRF);
+				set_brake(0, driveRB);
 			}
 			else
 			{
-				driveLF.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
-				driveLB.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
-				driveRF.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
-				driveRB.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
+				set_brake(2, driveLF);
+				set_brake(2, driveLB);
+				set_brake(2, driveRF);
+				set_brake(2, driveRB);
 			}
 			if(mainController.get_digital(E_CONTROLLER_DIGITAL_R1)) {
 				intakeHandler(190);
@@ -70,104 +70,109 @@ void opcontrol() {
 			if(mainController.get_digital(E_CONTROLLER_DIGITAL_L1)) { //350 and 210
 				macroHandler++;
 				if(macroHandler % 2 == 1) {
-					tray.set_brake_mode(E_MOTOR_BRAKE_COAST);
-					lift.set_brake_mode(E_MOTOR_BRAKE_COAST);
-					while(tray.get_position() < 200 || lift.get_position() < 120) {
-						lift.move_velocity(70);
-						pros::delay(200);
-						tray.move_velocity(175);
-						if(tray.get_position() > 200) {
-							tray.move_velocity(0);
-						}
-						if(lift.get_position() > 120) {
-							lift.move_velocity(0);
-						}
-						if(mainController.get_digital(E_CONTROLLER_DIGITAL_B)) {
-							break;
-						}
-						//printf("%f\n", lift.get_position());
-					}
-					tray.set_brake_mode(E_MOTOR_BRAKE_HOLD);
-					lift.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+					set_brake(1, tray);
+					set_brake(1, lift);
+					// while(tray.get_position() < 200 || lift.get_position() < 120) {
+					// 	lift.move_velocity(70);
+					// 	pros::delay(200);
+					// 	tray.move_velocity(175);
+					// 	if(tray.get_position() > 200) {
+					// 		tray.move_velocity(0);
+					// 	}
+					// 	if(lift.get_position() > 120) {
+					// 		lift.move_velocity(0);
+					// 	}
+					// 	if(mainController.get_digital(E_CONTROLLER_DIGITAL_B)) {
+					// 		break;
+					// 	}
+					// }
+					tray.move_absolute(200, 175);
+					pros::delay(200);
+					lift.move_absolute(120, 70);
+					set_brake(0, tray);
+					set_brake(0, lift);
 				}
 				else if(macroHandler % 2 == 0) {
-					tray.set_brake_mode(E_MOTOR_BRAKE_COAST);
-					lift.set_brake_mode(E_MOTOR_BRAKE_COAST);
-					while(tray.get_position() > 0 || lift.get_position() > 0) {
-						lift.move_velocity(-70);
-						pros::delay(100);
-						tray.move_velocity(-100);
-						if(tray.get_position() < 0) {
-							tray.move_velocity(0);
-						}
-						if(lift.get_position() < 0) {
-							lift.move_velocity(0);
-						}
-						if(mainController.get_digital(E_CONTROLLER_DIGITAL_B)) {
-							break;
-						}
-						//printf("%f\n", lift.get_position());
-					}
-					tray.set_brake_mode(E_MOTOR_BRAKE_HOLD);
-					lift.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+					set_brake(1, tray);
+					set_brake(1, lift);
+					// while(tray.get_position() > 0 || lift.get_position() > 0) {
+					// 	lift.move_velocity(-70);
+					// 	pros::delay(100);
+					// 	tray.move_velocity(-100);
+					// 	if(tray.get_position() < 0) {
+					// 		tray.move_velocity(0);
+					// 	}
+					// 	if(lift.get_position() < 0) {
+					// 		lift.move_velocity(0);
+					// 	}
+					// 	if(mainController.get_digital(E_CONTROLLER_DIGITAL_B)) {
+					// 		break;
+					// 	}
+					// }
+					tray.move_absolute(0, -175);
+					pros::delay(100);
+					lift.move_absolute(0, -70);
+					set_brake(0, tray);
+					set_brake(0, lift);
 				}
 			}
 			if(mainController.get_digital(E_CONTROLLER_DIGITAL_L2)) {
 				macroHandler2++;
 				if(macroHandler2 % 2 == 1) {
-					tray.set_brake_mode(E_MOTOR_BRAKE_COAST);
-					lift.set_brake_mode(E_MOTOR_BRAKE_COAST);
-					while(lift.get_position() < 205 || tray.get_position() < 200) {
-						lift.move_velocity(70);
-						pros::delay(100);
-						tray.move_velocity(175);
-						if(tray.get_position() > 205) {
-							tray.move_velocity(0);
-						}
-						if(lift.get_position() > 200) {
-							lift.move_velocity(0);
-						}
-						if(mainController.get_digital(E_CONTROLLER_DIGITAL_B)) {
-							break;
-						}
-						//printf("%f\n", lift.get_position());
-					}
+					set_brake(1, tray);
+					set_brake(1, lift);
+					// while(lift.get_position() < 205 || tray.get_position() < 200) {
+					// 	lift.move_velocity(70);
+					// 	pros::delay(100);
+					// 	tray.move_velocity(175);
+					// 	if(tray.get_position() > 205) {
+					// 		tray.move_velocity(0);
+					// 	}
+					// 	if(lift.get_position() > 200) {
+					// 		lift.move_velocity(0);
+					// 	}
+					// 	if(mainController.get_digital(E_CONTROLLER_DIGITAL_B)) {
+					// 		break;
+					// 	}
+					// }
+					tray.move_absolute(205, 175);
+					pros::delay(200);
+					lift.move_absolute(200, 70);
 				}
 				else if(macroHandler2 % 2 == 0) {
-					tray.set_brake_mode(E_MOTOR_BRAKE_COAST);
-					lift.set_brake_mode(E_MOTOR_BRAKE_COAST);
-					while(lift.get_position() > 0 || tray.get_position() > 0) {
-						lift.move_velocity(-70);
-						pros::delay(200);
-						tray.move_velocity(-100);
-						if(tray.get_position() < 0) {
-							tray.move_velocity(0);
-						}
-						if(lift.get_position() < 0) {
-							lift.move_velocity(0);
-						}
-						if(tray.get_position() < 0 && lift.get_position() < 0) {
-							break;
-						}
-						//printf("%f\n", lift.get_position());
-					}
+					set_brake(1, tray);
+					set_brake(1, lift);
+					// while(lift.get_position() > 0 || tray.get_position() > 0) {
+					// 	lift.move_velocity(-70);
+					// 	pros::delay(200);
+					// 	tray.move_velocity(-100);
+					// 	if(tray.get_position() < 0) {
+					// 		tray.move_velocity(0);
+					// 	}
+					// 	if(lift.get_position() < 0) {
+					// 		lift.move_velocity(0);
+					// 	}
+					// 	if(tray.get_position() < 0 && lift.get_position() < 0) {
+					// 		break;
+					// 	}
+					// }
 				}
-				tray.set_brake_mode(E_MOTOR_BRAKE_HOLD);
-				lift.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+				tray.move_absolute(0, -175);
+				pros::delay(100);
+				lift.move_absolute(0, -70);
+				set_brake(0, tray);
+				set_brake(0, lift);
 			}
 			if(tray.get_position() < 200) {
-				intakeL.set_brake_mode(E_MOTOR_BRAKE_HOLD);
-				intakeR.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+				set_brake(0, intakeL);
+				set_brake(0, intakeR);
 			}
 			else {
-				intakeL.set_brake_mode(E_MOTOR_BRAKE_COAST);
-				intakeR.set_brake_mode(E_MOTOR_BRAKE_COAST);
+				set_brake(1, intakeL);
+				set_brake(1, intakeR);
 			}
 			int trayPos = mainController.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y);
 			trayHandler(trayPos);
-			//printf("Temperature: %f \n", lift.get_temperature());
-			//printf("%f", tray.get_position());
 			pros::delay(20);
 		}
-		autonhandler();
 }
