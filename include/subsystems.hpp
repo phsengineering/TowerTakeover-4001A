f2e0460a88c49753d45478c19b0816171c2d0849
javathunderman <javathunderman@outlook.com>
2019-12-08 12:52:15 -0500

    more unstable

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -15,7 +15,7 @@ extern ADIEncoder mEncoder;
 enum brake_modes {HOLD = 0, COAST = 1, BRAKE = 2};
 static int part = 0;
 static int auton = 7; //runs case 7
-static double chassisWidth = 5.0;
+static double chassisWidth = 2.5;
 static bool debug = false;
 static double lastEncoderValueL = 0;
 static double lastEncoderValueM = 0;
--- src/okapi_auton.cpp
+++ src/okapi_auton.cpp
@@ -7,53 +7,39 @@ const double drivekI = 0.0000;
 const double drivekD = 0.0000;
 const double drivekBias = 0;
 const double maxVel = 5.00;
-const double maxAccel = 3.00;
+const double maxAccel = 1.00;
 const double maxJerk = 9.00;
 const double odomTrack = 0.0;
 const double odomMiddleDist = 0.0;
 const double drivemoveThreshold = 0.0;
 const double driveturnThreshold = 0.0;
 //const double driveVelDelta = 0.001;
+Logger logger = Logger(TimeUtilFactory::createDefault().getTimer(), // It needs a Timer
+        "/ser/sout", Logger::LogLevel::debug);
 auto odom = ChassisControllerBuilder()
-    .withMotors({1, 2}, {3, -4})
-    .withGearset(AbstractMotor::gearset::blue)
-    .withSensors({'E', 'F', false}, {'A', 'B', false}, {'C', 'D', true})
-    .withDimensions({{3.25, 9}, imev5BlueTPR})
+    .withMotors({4, 3}, {2, 1})
+    .withGearset({AbstractMotor::gearset::blue, 5/3})
+    .withSensors(ADIEncoder('E', 'F', false), ADIEncoder('A', 'B', false), ADIEncoder('C', 'D', false))
+    .withDimensions({{2.75, 5.125, 4.25, 2.75}, imev5BlueTPR})
     //.withGearset(5.0/3.0)
     .withGains(
-        { 0.001, 0, 0.0001 }, // Distance controller gains
-        { 0.001, 0, 0.0001 }, // Turn controller gains
-        { 0.001, 0, 0.0001 }  // Angle controller gains (helps drive straight)
+        { 0.001, 0, 0.0000 }, // Distance controller gains
+        { 0.001, 0, 0.0000 }, // Turn controller gains
+        { 0.0001, 0, 0.0000 }  // Angle controller gains (helps drive straight)
     )
     //.withOdometry({2.75, odomTrack, odomMiddleDist, 2.75}, drivemoveThreshold, driveturnThreshold)
     .withOdometry(okapi::StateMode::FRAME_TRANSFORMATION, 0_ft, 0_deg, 0.00001_mps)
-    //.withLogger(const std::shared_ptr<Logger>& ilogger)
+    //.withLogger()
     .buildOdometry();
-
 auto pathgen = AsyncMotionProfileControllerBuilder()
     .withLimits({maxVel, maxAccel, maxJerk})
     //.withOutput(drive)
     .withOutput(odom)
     .buildMotionProfileController();
 
-void settle(){
-  pathgen->waitUntilSettled();
-}
-
-void beans(){
-  pathgen->generatePath({{1_ft, 0_ft, 0_deg}, {5_ft, 0_ft, 0_deg} }, "bean1");
-  pathgen->generatePath({{5_ft, 0_ft, 0_deg}, {2_ft, 0_ft, 0_deg}, {1_ft, 2_ft, 0_deg}}, "bean2");
-  pathgen->generatePath({{1_ft, 2_ft, 0_deg}, {5_ft, 5_ft, 0_deg}}, "bean3");
-}
-
 void movingbeans() {
-  //intakeHandler(200);
-  beans();
-  pathgen->setTarget("bean1");
-  settle();
-  pathgen->setTarget("bean2");
-  settle();
-  pathgen->setTarget("bean3");
-  settle();
-  //intakeHandler(0);
+  odom->turnAngle(180_deg);
+  odom->waitUntilSettled();
+  //pathgen->setTarget("A");
+  //pathgen->waitUntilSettled();
 }
