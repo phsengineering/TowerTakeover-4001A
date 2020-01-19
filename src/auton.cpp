1e8a1cda36cfe4ec72ca97803fe40c21b9aa2680
javathunderman <javathunderman@outlook.com>
2020-01-19 13:53:48 -0500

    finally working scales

--- src/auton.cpp
+++ src/auton.cpp
@@ -9,8 +9,8 @@ std::shared_ptr<okapi::OdomChassisController> chassis = ChassisControllerBuilder
          { 0.00022, 0, 0.0000 }  // Angle controller gains (helps drive straight)
      )
     .withSensors({'E', 'F', true}, {'A', 'B', false}, {'C', 'D', true})
-    .withDimensions(AbstractMotor::gearset::blue, {{3.25_in, 9.625_in}, imev5BlueTPR})
-    .withOdometry({{2.75_in, 5_in, 4.375_in, 2.75_in}, quadEncoderTPR}) // use the same scales as the chassis (above)
+    .withDimensions(AbstractMotor::gearset::blue, {{2.75_in, 4.25_in, 4.375_in, 2.75_in}, quadEncoderTPR})
+    .withOdometry() // use the same scales as the chassis (above)
     .withLogger(std::make_shared<Logger>(
         TimeUtilFactory::createDefault().getTimer(), // It needs a Timer
         "/ser/sout", // Output to the PROS terminal
@@ -30,45 +30,5 @@ void autonhandler() {
     chassis->driveToPoint({0_ft, -2_ft});
 }
 void mptest() {
-  //ChassisScales scales = ChassisScales({3.25, 9.75}, (imev5BlueTPR*(3/5)));
-  Logger logger = Logger(TimeUtilFactory::createDefault().getTimer(), // It needs a Timer
-          "/ser/sout", Logger::LogLevel::debug);
-  TimeUtil test = TimeUtilFactory::createDefault();
-  auto chassis = ChassisControllerBuilder()
-      .withMotors({4, 3}, {2, 1})
-      .withDimensions(AbstractMotor::gearset::blue, {{3.25_in, 9.75_in}, imev5BlueTPR*(5/3)})
-      .withSensors({'E', 'F', false}, {'A', 'B', false}, {'C', 'D', false})
-      .withOdometry({{2.76_in, 5_in, 4.5_in, 2.76_in}, quadEncoderTPR})
-      .buildOdometry(); // build an odometry chassis
-
-
-      auto profileController = AsyncMotionProfileControllerBuilder()
-          .withLimits({1.368, 5.5, 6.155}) //max vel, max accel, max jerk
-          .withOutput(chassis)
-          .buildMotionProfileController();
-
-      profileController->generatePath({
-      {0_ft, 0_ft, 0_deg},
-      {7_ft, 0_ft, 0_deg}},  // Profile starting position, this will normally be (0, 0, 0) // The next point in the profile, 3 feet forward
-      "A" // Profile name
-    );
-      profileController->generatePath({
-      {0_ft, 0_ft, 0_deg},  // Profile starting position, this will normally be (0, 0, 0)
-      {2_ft, 0_ft, 0_deg}}, // The next point in the profile, 3 feet forward
-      "B" // Profile name
-    );
-      profileController->generatePath({
-      {0_ft, 0_ft, 0_deg},  // Profile starting position, this will normally be (0, 0, 0)
-      {4_ft, 5.2_ft, 180_deg}}, // The next point in the profile, 3 feet forward
-      "C" // Profile name
-);
-//start autonomous here
-      profileController->setTarget("A");
-      profileController->waitUntilSettled();
-      profileController->setTarget("B",true);
-      profileController->setTarget("C",true);
-      profileController->waitUntilSettled();
-      //profileController->setTarget("D");
-      //profileController->waitUntilSettled();
-      //profileController->setTarget("E",true);
+    chassis->driveToPoint({2_ft, 2_ft});
 }
